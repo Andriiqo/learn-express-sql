795fd73c5c3c6f5eabda1df2cc8237a439935265
Andriiqo <anreiblashchuk@gmail.com>
2024-01-16 21:37:29 +0400

    fix: params route

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,11 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT
 
-const urlencodedParser = express.urlencoded({extended: false});
-
 app.use(express.static(path.join(__dirname, '/public'), {extensions: ['html']}));
 
-app.get('/', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-app.post('/', urlencodedParser, (req, res) => {
-    console.log('req', req.body)
-    if(!req.body?.userName || !req.body?.userAge) return res.sendStatus(400)
-
-    res.send(`${req.body.userName} - ${req.body.userAge}`);
-})
+app.get("/cars/:tesla/model/:model", function (request, response) {
+    response.send(`марка: ${request.params['tesla']}, модель: ${request.params['model']}`)
+  });
 
 app.listen(port, () => {
     console.log(`Server has been started on ${port} port`)
