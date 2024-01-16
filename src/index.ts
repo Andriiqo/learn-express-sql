b5d6a50b03f65579a60a27698058eb9e2ab610f3
Andriiqo <anreiblashchuk@gmail.com>
2024-01-16 21:24:28 +0400

    learn: post-req

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,20 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.PORT
 
+const urlencodedParser = express.urlencoded({extended: false});
+
 app.use(express.static(path.join(__dirname, '/public'), {extensions: ['html']}));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.use('/test', (req: Request, res: Response) => {
-    const id = req.query.id;
-    const userName = req.query.name;
-    res.send(`<h1>Информация</h1><p>id: ${id}</p><p>name: ${userName}</p>`);
-})
+app.post('/', urlencodedParser, (req, res) => {
+    console.log('req', req.body)
+    if(!req.body?.userName || !req.body?.userAge) return res.sendStatus(400)
 
+    res.send(`${req.body.userName} - ${req.body.userAge}`);
+})
 
 app.listen(port, () => {
     console.log(`Server has been started on ${port} port`)
